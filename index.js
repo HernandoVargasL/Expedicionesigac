aafbfae6b247b561b64fd9eefb4704f912a7ee09
HernandoVargasL <91139093+HernandoVargasL@users.noreply.github.com>
2022-11-17 11:25:40 -0500

    ajustes raiz 2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ if (document.querySelector(".navbarigac")) {
     navbarigac.innerHTML = `
     <div class="logos">
         <div class="navbar-brand-igac">
-            <a href='https://www.igac.gov.co' target='_blank'><img alt='Logo IGAC' src='/images/logo-igac.png'/></a>
+            <a href='https://www.igac.gov.co' target='_blank'><img alt='Logo IGAC' src='images/logo-igac.png'/></a>
         </div>
         <div class="logoigac">
             <a href="index.html"><img src="images/logo.svg" alt="Expediciones Igac"></a>
